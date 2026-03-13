fa8f853dca90f7f4837eb90ea53046b9e64c33f0
yaoxiao <yaoxiao@corp.netease.com>
2026-03-13 15:52:46 +0800

    新增OC nim uikit de磨ui源码

---
+++ iOS demo/OC demo/含ui demo/nim uikit/ui源码集成/IMUIKitOC/IMUIKitOCExample/IMUIKitOCExample/AppKey.h
@@ -0,0 +1,12 @@
+
+// Copyright (c) 2022 NetEase, Inc. All rights reserved.
+// Use of this source code is governed by a MIT license that can be
+// found in the LICENSE file.
+
+#ifndef AppKey_h
+#define AppKey_h
+
+/// IM key
+static NSString *const AppKey = @"4727023efa991d31d61b3b32e819bd5b";
+
+#endif /* AppKey_h */
